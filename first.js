f2363cd9c645d9bf5d04cadf08c78c06251644a7
Your Name <your_email@example.com>
2025-01-27 10:52:22 +0530

    next commit

--- first.js
+++ first.js
@@ -2,9 +2,20 @@ console.log(name="rohit")
 console.log(age=13)
 console.log(balance=2000)
 console.log("raj modi")
+console.log("hello")
 //cd git
 //git add.
 //git status
 //make changes and commit them
 //git commit -m "first"
 //gti log
+//git branch
+//git checkout -b"feature"
+//git branch
+//git checkout master
+// git merge feature
+// git branch - D feature
+//git checkout -b Bug
+//git commit -m "first bug"
+console.log("monfey honey")
+console.log("monfey hobvhney")

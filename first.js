cd37162ff29cbb06f34a7c4258acb1b15b55d23d
Your Name <your_email@example.com>
2025-01-27 10:50:00 +0530

    first bug

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 console.log(name="rohit")
 console.log(age=13)
 console.log(balance=2000)
+console.log("raj modi")
+//cd git
+//git add.
+//git status
+//make changes and commit them
+//git commit -m "first"
+//gti log
